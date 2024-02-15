4a62f84a866ad76e1b988115573c71bd319a0613
Marcos <burgossaavedra.marcos@gmail.com>
2024-02-15 16:36:23 +1000

    modify process_data.py

--- cybulde/process_data.py
+++ cybulde/process_data.py
@@ -8,7 +8,7 @@ def process_data(config: Config) -> None:
     print(config)
 
     my_dummy_secret = access_secret_version("mlopsproject-413606", "dummy-secret")
-    print(f"{my_dummy_secret}")
+    print(f"{my_dummy_secret=}")
 
 if __name__ == "__main__":
     process_data()  # type: ignore
