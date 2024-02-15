dbba40cf5ec50dc6ba830d045cb14907ad2f2d07
Marcos <burgossaavedra.marcos@gmail.com>
2024-02-15 16:16:17 +1000

    modify process_data.py

--- cybulde/utils/gcp_utils.py
+++ cybulde/utils/gcp_utils.py
@@ -7,7 +7,7 @@ def access_secret_version(project_id: str, secret_id: str, version_id: str = "1"
     """
 
     client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}"
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
     response = client.access_secret_version(request={"name": name})
     payload = response.payload.data.decode("UTF-8")
     return payload
