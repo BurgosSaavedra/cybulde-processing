2087776521d3b5d71aa22773f82664906f541059
Marcos <burgossaavedra.marcos@gmail.com>
2024-02-16 09:04:10 +1000

    modify process_data.py

--- cybulde/process_data.py
+++ cybulde/process_data.py
@@ -9,7 +9,7 @@ def process_data(config: Config) -> None:
     version = "v1"
     data_local_save_dir = "./data/raw"
     dvc_remote_repo = "https://github.com/BurgosSaavedra/cybulde-data.git"
-    dvc_data_folder = "data_raw"
+    dvc_data_folder = "data/raw"
     github_user_name = "BurgosSaavedra"
     github_access_token = access_secret_version("mlopsproject-413606", "cybulde-data-github-access-token")
 
