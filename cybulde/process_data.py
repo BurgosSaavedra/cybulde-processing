414bb5fc5d65a2ceaf2467b63579cd161267126d
Marcos <burgossaavedra.marcos@gmail.com>
2024-02-16 09:50:58 +1000

    modify process_data.py

--- cybulde/process_data.py
+++ cybulde/process_data.py
@@ -6,6 +6,8 @@ from cybulde.utils.gcp_utils import access_secret_version
 
 @get_config(config_path="../configs", config_name="data_processing_config")
 def process_data(config: DataProcessingConfig) -> None:
+    github_access_token = access_secret_version(config.infrastructure.project_id, config.github_access_token_secret_id)
+    
     get_raw_data_with_version(
         version=config.version,
         data_local_save_dir=config.data_local_save_dir,
