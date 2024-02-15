c4fe69fddf09c06e5942f43846b8edd276e7058b
Marcos <burgossaavedra.marcos@gmail.com>
2024-02-16 09:42:41 +1000

    modify data_processing_config_schema.py, gcp_schema.py, data_processing_config.yaml, process_data.py, config_utils.py

--- cybulde/config_schemas/infrastructure/gcp_schema.py
+++ cybulde/config_schemas/infrastructure/gcp_schema.py
@@ -1,12 +1,10 @@
 from hydra.core.config_store import ConfigStore
 from pydantic.dataclasses import dataclass
 
-
 @dataclass
-class Config:
-    helle: str = "world"
-
+class GCPConfig:
+    project_id: str = "mlopsproject-413606"
 
 def setup_config() -> None:
     cs = ConfigStore.instance()
-    cs.store(name="config_schema", node=Config)
+    cs.store(name="gcp_config_schema", node=GCPConfig)
--- cybulde/process_data.py
+++ cybulde/process_data.py
@@ -1,11 +1,14 @@
-from cybulde.config_schemas.config_schema import Config
+from cybulde.config_schemas.data_processing_config_schema import DataProcessingConfig
 from cybulde.utils.config_utils import get_config
 from cybulde.utils.data_utils import get_raw_data_with_version
 from cybulde.utils.gcp_utils import access_secret_version
 
 
-@get_config(config_path="../configs", config_name="config")
-def process_data(config: Config) -> None:
+@get_config(config_path="../configs", config_name="data_processing_config")
+def process_data(config: DataProcessingConfig) -> None:
+    print(config)
+    return
+
     version = "v1"
     data_local_save_dir = "./data/raw"
     dvc_remote_repo = "https://github.com/BurgosSaavedra/cybulde-data.git"
